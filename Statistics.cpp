bd36833194e0a51fe6a9155d19d42d3b9cafefbd
matt-mtt <m.moutot10@gmail.com>
2021-05-05 15:23:00 +0200

    stats propre

--- Statistics.cpp
+++ Statistics.cpp
@@ -5,33 +5,25 @@
     copyright            : (C) 2021
     e-mail               : matthieu.moutot@insa-lyon.fr ; mettez vos emails
 *************************************************************************/
-<<<<<<< HEAD
+
 //---- Réalisation de la classe <Statistics> (fichier Statistics.cpp) ---------
 //-------------------------------------------------------- Include système
-=======
+
 
 #include <Statistics.h>
->>>>>>> 7ad63f031b6934a27b0920217dbd4982dafb5508
+
 #include <iostream>
 #include <string>
 #include <vector>
 #include <unordered_map>
-//------------------------------------------------------ Include personnel
 
-
-//------------------------------------------------------------- Constantes
-
-//----------------------------------------------------------------- PUBLIC
-
-<<<<<<< HEAD
-//----------------------------------------------------- Méthodes publiques
-void Statistics::CalculateSimilarity(String sensorId, Date date)
+int Statistics::calculateAirQuality(float latitude, float longitude, int radius, Date date);
 {
-  vector<Measurement> allMeasurements;
-  unordered_map<>
-=======
+
+
 void Statistics::calculateSimilarity(String sensorID, Date date)
 {
-    
->>>>>>> 7ad63f031b6934a27b0920217dbd4982dafb5508
+    vector<Measurement> allMeasurements;
+    unordered_map<>
+
 }
