f88ddcc620860570511fba9f8a72eeafd6b5c676
Gustavo Bertoldi <gg.bertoldi@hotmail.com>
2021-05-05 15:12:13 +0200

    ..

--- Statistics.cpp
+++ Statistics.cpp
@@ -5,3 +5,18 @@
     copyright            : (C) 2021
     e-mail               : matthieu.moutot@insa-lyon.fr ; mettez vos emails
 *************************************************************************/
+//---- Réalisation de la classe <Statistics> (fichier Statistics.cpp) ---------
+//-------------------------------------------------------- Include système
+#include <iostream>
+//------------------------------------------------------ Include personnel
+
+
+//------------------------------------------------------------- Constantes
+
+//----------------------------------------------------------------- PUBLIC
+
+//----------------------------------------------------- Méthodes publiques
+void Statistics::CalculateSimilarity(String sensorId, Date date)
+{
+  
+}
