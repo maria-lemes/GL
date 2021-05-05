75bc797f73f83990dc65e11a58b709942908c0cb
Gustavo Bertoldi <gg.bertoldi@hotmail.com>
2021-05-05 17:46:14 +0200

    Minor fix

--- Measurement.h
+++ Measurement.h
@@ -11,7 +11,82 @@
 
 #include <iostream>
 using namespace std;
+struct date{
+    int year;
+    int month;
+    int day;
+    int hour;
+    int minute;
+    int second;
 
+
+
+    inline bool operator==(date b)
+    {
+      return year==b.year && month==b.month && day==b.day && hour==b.hour &&
+      minute==b.minute && second==b.second;
+    }
+
+    inline bool operator<(date b)
+    {
+      if (year < b.year)
+      {
+        return true;
+      } else if (year > b.year)
+      {
+        return false;
+      } else {
+        if (month < b.month)
+        {
+          return true;
+        } else if (month > b.month)
+        {
+          return false;
+        }
+        else{
+          if (day < b.day)
+          {
+            return true;
+          } else if (day > b.day)
+          {
+            return false;
+          }
+          else{
+            if (hour < b.hour)
+            {
+              return true;
+            } else if (hour > b.hour)
+            {
+              return false;
+            }
+            else{
+              if (minute < b.minute)
+              {
+                return true;
+              } else if (minute > b.minute)
+              {
+                return false;
+              }
+              else{
+                if (second < b.second)
+                {
+                  return true;
+                } else if (second > b.second)
+                {
+                  return false;
+                }
+                else{
+                  return false;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  
 class Measurement
 {
     public:
@@ -20,82 +95,9 @@ class Measurement
 
         Measurement(const Measurement & oneMeasurement);
 
-        struct date{
-            int year;
-            int month;
-            int day;
-            int hour;
-            int minute;
-            int second;
-
-
-
-            inline bool operator==(date b)
-            {
-              return year==b.year && month==b.month && day==b.day && hour==b.hour &&
-              minute==b.minute && second==b.second;
-            }
-
-            inline bool operator<(date b)
-            {
-              if (year < b.year)
-              {
-                return true;
-              } else if (year > b.year)
-              {
-                return false;
-              } else {
-                if (month < b.month)
-                {
-                  return true;
-                } else if (month > b.month)
-                {
-                  return false;
-                }
-                else{
-                  if (day < b.day)
-                  {
-                    return true;
-                  } else if (day > b.day)
-                  {
-                    return false;
-                  }
-                  else{
-                    if (hour < b.hour)
-                    {
-                      return true;
-                    } else if (hour > b.hour)
-                    {
-                      return false;
-                    }
-                    else{
-                      if (minute < b.minute)
-                      {
-                        return true;
-                      } else if (minute > b.minute)
-                      {
-                        return false;
-                      }
-                      else{
-                        if (second < b.second)
-                        {
-                          return true;
-                        } else if (second > b.second)
-                        {
-                          return false;
-                        }
-                        else{
-                          return false;
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-        }
-
         string getSensorID();
+
+        string getAttribute();
 
         string getAttribute();
 
